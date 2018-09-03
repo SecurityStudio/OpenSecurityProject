00a222b849b1a3ad3eac09242dc00846d04f7522
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-09-04 01:22:43 +0430

    User entity

--- web/src/main/java/com/npci/shirotutorial/security/model/entity/User.java
+++ web/src/main/java/com/npci/shirotutorial/security/model/entity/User.java
@@ -6,14 +6,18 @@ import java.sql.Timestamp;
 import java.util.Set;
 
 @Entity
-@Table(name = "SECURITY_USER")
+@Table(name = "SECURITY_USER", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"USERNAME"})
+})
 @SequenceGenerator(name = "SEQ_GENERATOR", sequenceName = "USER_SEQ")
 @NamedQueries({
         @NamedQuery(name = User.FIND_ALL, query = "select t from User t"),
+        @NamedQuery(name = User.FIND_BY_USERNAME, query = "SELECT u FROM User u WHERE u.username = :username"),
 })
 public class User {
 
     public static final String FIND_ALL = "User.findAll";
+    public static final String FIND_BY_USERNAME = "User.findByUsername";
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GENERATOR")
@@ -80,5 +84,13 @@ public class User {
 
     public void setVersion(Timestamp version) {
         this.version = version;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
